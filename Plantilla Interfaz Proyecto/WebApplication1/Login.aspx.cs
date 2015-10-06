0c73a661f62334a4d6cd95da103f443755a63f8f
unknown <b30640@ECCI-LBW-106-07.ecci.ucr.ac.cr>
2015-10-06 11:06:47 -0600

    inicio mod login

---
+++ Plantilla Interfaz Proyecto/WebApplication1/Login.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using WebApplication1.App_Code;
+
+namespace WebApplication1
+{
+
+    public partial class Login : System.Web.UI.Page
+    {
+        
+        protected ControladoraRH controladora = new ControladoraRH();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        protected void BtnLogin_Click(object sender, EventArgs e)
+        {
+            //Do somthing
+            string nombreUsuario = user.Text;
+            string contra = password.Text;
+            if(controladora.usuarioValido(nombreUsuario, contra))
+            {
+                Response.Write("<script>alert('entro');</script>");
+            }
+            else
+            {
+                Response.Write("<script>alert('datos incorrectos');</script>");
+            }
+            
+            
+        } 
+    }
+}
