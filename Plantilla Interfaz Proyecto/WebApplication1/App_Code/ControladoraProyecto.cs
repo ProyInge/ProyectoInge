5fc6700c43f1ea4596259ef3776038729f397eea
dmunoz8 <daniel33045@gmail.com>
2015-10-06 09:08:06 -0600

    ahora si xD

--- Plantilla Interfaz Proyecto/WebApplication1/App_Code/ControladoraProyecto.cs
+++ Plantilla Interfaz Proyecto/WebApplication1/App_Code/ControladoraProyecto.cs
@@ -7,8 +7,7 @@ namespace WebApplication1.App_Code
 {
     public class ControladoraProyecto
     {
-        SqlConnection con = new SqlConnection("Server=eccibdisw; Initial Catalog= g4inge; Integrated Security=SSPI");
-        DataTable tabla = new DataTable();
+        
 
     }
 }
