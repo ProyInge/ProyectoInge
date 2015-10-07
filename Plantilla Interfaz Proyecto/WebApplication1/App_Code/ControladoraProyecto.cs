778829caee8f858ea9b1788ef58e1c502df7c8af
angelicaF <angelica.fallas@ucr.ac.cr>
2015-10-07 09:58:57 -0600

    proyecto-contr
    control

--- Plantilla Interfaz Proyecto/WebApplication1/App_Code/ControladoraProyecto.cs
+++ Plantilla Interfaz Proyecto/WebApplication1/App_Code/ControladoraProyecto.cs
@@ -7,7 +7,40 @@ namespace WebApplication1.App_Code
 {
     public class ControladoraProyecto
     {
+        //private Proyecto nuevoProyecto; //instanciade la entidad proyecto  
+        //private ControladoraBDProyecto controladoraBDProyecto; // instancia de controladora de BD de proyecto
 
-        
+        public ControladoraProyecto()
+        {
+            //controladoraBDProyecto =new ControladoraProyectoBDProyecto();
+        }
+
+        public string insertar(Object[] datos)
+        {
+            string msjResultado = "";
+
+            return msjResultado;
+        }
+        public string modificar(Object[] datos, Object[] datosOriginales)
+        {
+            string msjResultado = "";
+            return msjResultado;
+        }
+        public string eliminar(Object[] datos)
+        {
+            string msjResultado = "";
+            return msjResultado;
+        }
+        public string consultar(Object[] datos)
+        {
+            string msjResultado = "";
+            return msjResultado;
+        }
+        public string consultarProyectos()
+        {
+            string msjResultado = "";
+            return msjResultado;
+        }
+
     }
 }
