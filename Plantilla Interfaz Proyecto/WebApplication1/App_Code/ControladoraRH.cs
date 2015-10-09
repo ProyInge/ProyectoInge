4eb49ee1cdbb6c005c936d53e08686f9dd764375
David Solano <desolanom@gmail.com>
2015-10-09 10:43:36 -0600

    EntidadRH 1.1

--- Plantilla Interfaz Proyecto/WebApplication1/App_Code/ControladoraRH.cs
+++ Plantilla Interfaz Proyecto/WebApplication1/App_Code/ControladoraRH.cs
@@ -19,5 +19,25 @@ namespace WebApplication1.App_Code
         {
             return controlBD.usuarioValido(nombreUsuario, contra);
         }
+        public bool insertaRH(int cedula, String nombre, String pApellido, String sApellido, String correo, String nomUsuario, String contra, char perfil, int idProy, String rol)
+        {
+            RecursoH insRH = new RecursoH(cedula, nombre, pApellido, sApellido, correo, nomUsuario, contra, perfil, idProy, rol);
+            return true;
+            //return controlBD.insertaRH(insRH);
+        }
+
+        public bool modificaRH(int cedula, String nombre, String pApellido, String sApellido, String correo, String nomUsuario, String contra, char perfil, int idProy, String rol)
+        {
+            RecursoH modRH = new RecursoH(cedula, nombre, pApellido, sApellido, correo, nomUsuario, contra, perfil, idProy, rol);
+            //return controlBD.modificaRH(insRH);
+            return true;
+        }
+
+        public bool eliminaRH(int cedula)
+        {
+            //return controlBD.eliminaRH(cedula);
+            return true;
+        }
+
     }
 }
