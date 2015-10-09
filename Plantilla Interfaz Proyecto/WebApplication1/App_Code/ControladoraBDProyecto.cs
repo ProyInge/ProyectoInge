cc70a7fbbec2cca889db4475a2b8c53937e30a33
dmunoz8 <daniel33045@gmail.com>
2015-10-09 09:23:48 -0600

    BD Proyecto

---
+++ Plantilla Interfaz Proyecto/WebApplication1/App_Code/ControladoraBDProyecto.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.App_Code
+{
+    public class ControladoraBDProyecto
+    {
+        public ControladoraBDProyecto()
+        {
+
+        }
+        public string insertarProyecto()
+        {
+            string resultado = "";
+            return resultado;
+        }
+        public string modificarProyecto()
+        {
+            string resultado = "";
+            return resultado;
+        }
+        public string eliminarProyecto()
+        {
+            string resultado = "";
+            return resultado;
+        }
+        public string consultarProyecto()
+        {
+            string resultado = "";
+            return resultado;
+        }
+        public string consultar_total_Proyecto()
+        {
+            string resultado = "";
+            return resultado;
+        }
+
+    }
+}
