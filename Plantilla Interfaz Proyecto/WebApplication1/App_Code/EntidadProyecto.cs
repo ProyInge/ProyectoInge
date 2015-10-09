77b12736c870ddb1815a8dd0b92df08421039009
dmunoz8 <daniel33045@gmail.com>
2015-10-09 08:56:15 -0600

    Modulo Proyecto

---
+++ Plantilla Interfaz Proyecto/WebApplication1/App_Code/EntidadProyecto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.App_Code
+{
+    public class EntidadProyecto
+    {
+        string nombre;
+        string objetivo;
+        char estado;
+        DateTime fecha;
+        string nombreOf;
+        string representante;
+        string correoOf;
+        int telefonoOf;
+
+        public EntidadProyecto(string nombreP,string obj, char est, DateTime fechaP, string nomOf, string rep, string email, int telOf)
+        {
+            nombre = nombreP;
+            objetivo = obj;
+            estado = est;
+            fecha = fechaP;
+            nombreOf = nomOf;
+            representante = rep;
+            correoOf = email;
+            telefonoOf = telOf;
+        }
+
+        public char getEstado()
+        {
+            return estado;
+        }
+    }
+}
--- Plantilla Interfaz Proyecto/WebApplication1/Proyecto.aspx.cs
+++ Plantilla Interfaz Proyecto/WebApplication1/Proyecto.aspx.cs
@@ -4,13 +4,14 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using WebApplication1.App_Code;
 
 namespace WebApplication1
 {
     public partial class Proyecto : System.Web.UI.Page
     {
-
-
+        private ControladoraProyecto controladoraProyecto;
+       
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -45,8 +46,18 @@ namespace WebApplication1
 
         protected void btnAceptar_Insertar(object sender, EventArgs e)
         {
-            string obj = objetivo.Text;
-            string nombre = nombreProyecto.Value;
+            /*string obj = objetivo.Text;
+            string nombreP = nombreProyecto.Value;
+            string est = barraEstado.Value;
+            string fechaP = calendario.Value;
+            string nomOf = nombreOficina.Value;
+            string rep = representante.Value;
+            string email = correoOficina.Value;
+            string telOf = telefonoOficina.Value;*/
+            char est = barraEstado.Value[0];
+            Object[] dat = new Object[8];
+            dat[0] = nombreProyecto.Value; 
+           
         }
         
     }
