5fb9b5fd46b79b4519aa4559b6c4eab64bbb51c5
angelicaF <angelica.fallas@ucr.ac.cr>
2015-10-15 18:47:44 -0600

    mljl

--- Plantilla Interfaz Proyecto/WebApplication1/App_Code/ControladoraBDProyecto.cs
+++ Plantilla Interfaz Proyecto/WebApplication1/App_Code/ControladoraBDProyecto.cs
@@ -11,8 +11,9 @@ namespace WebApplication1.App_Code
     {
 
         private AccesoBaseDatos baseDatos;
-        String conexion = "Server=eccibdisw; Initial Catalog= g4inge; Integrated Security=SSPI";        
-        
+        //String conexion = "Server=eccibdisw; Initial Catalog= g4inge; Integrated Security=SSPI";        
+        String conexion = "Server=DESKTOP-FRM9QAR\\SQLEXPRESS; Initial Catalog= eccibdisw; Integrated Security=SSPI";
+
         public ControladoraBDProyecto()
         {
             baseDatos = new AccesoBaseDatos();
@@ -258,7 +259,7 @@ namespace WebApplication1.App_Code
             string resultado = "Exito";
             string consulta = "";
 
-            Object[] datos = new Object[9];
+            Object[] datos = new Object[11];
 
             string res = "";
 
@@ -268,7 +269,7 @@ namespace WebApplication1.App_Code
             try
             {
                 //consulta = "SELECT  objetivo   FROM Proyecto  WHERE nombre= "+ "'"+nombreP+ "';";
-                consulta = "select p.objetivo,p.estado, p.fechaAsignacion, o.nombre, o.representante, o.correo, tel.numero from Proyecto p, OficinaUsuaria o, TelefonoOficina tel where p.nombre = '" + nombreP + "' and p.id = o.idProyecto and tel.idCliente = o.id;";
+                consulta = "select p.objetivo,p.estado, p.fechaAsignacion, o.nombre, o.representante, o.correo, tel.numero, u.cedula, u.pNombre from Proyecto p, OficinaUsuaria o, TelefonoOficina tel, Usuario u where p.nombre = '"+ nombreP + "' and p.id = o.idProyecto and tel.idCliente = o.id and u.idProy=p.id;";
 
                 SqlDataReader reader = baseDatos.ejecutarConsulta(consulta);
                 try
@@ -278,13 +279,16 @@ namespace WebApplication1.App_Code
                         datos[0] = "";                  //nombre
                         datos[1] = reader.GetString(0); //objetivo
                         datos[2] = reader.GetString(1); //estado
-                        datos[3] = null;                //fechaAsgnacion
+                        datos[3] = null; // reader.GetString(2); ;//Convert.ToDateTime(reader.GetString(2));  //fechaAsgnacion
                         datos[4] = reader.GetString(3); //nombreOficina
                         datos[5] = reader.GetString(4); // representante
                         datos[6] = reader.GetString(5); //cooreoOficina
-                        datos[7] = reader.GetInt32(6);//Convert.ToInt32(reader.GetString(6)); //telOficina
-                        datos[8] = -1;//reader.GetInt32(7);// Convert.ToInt32(reader.GetString(7)); //lider
-                        objPro = new EntidadProyecto(datos);
+                        datos[7] = reader.GetInt32(6);//Convert.ToInt32(reader.GetString(6)); //telOficina                      
+                        datos[8] = reader.GetInt32(7);//cedula lider                      
+                        datos[9] = reader.GetString(8);//nombreLider
+                        datos[10] = 0;//reader.GetInt32(9);//tel2
+
+                       objPro = new EntidadProyecto(datos);
                     }
                 }
                 catch (SqlException ex)
--- Plantilla Interfaz Proyecto/WebApplication1/Proyecto.aspx.cs
+++ Plantilla Interfaz Proyecto/WebApplication1/Proyecto.aspx.cs
@@ -173,7 +173,7 @@ namespace WebApplication1
                      if ((!string.IsNullOrWhiteSpace(tel2.Value) && !(tel2.Value.Equals(telefonoOficina.Value))) || (string.IsNullOrWhiteSpace(tel2.Value)))
                      {
 
-                         Object[] dat = new Object[9];
+                         Object[] dat = new Object[11];
                          Object[] vacio = new Object[1];
                          string l = lider.Value;
                          string ll = l.Substring(0, 9);
@@ -184,8 +184,11 @@ namespace WebApplication1
                          dat[4] = nombreOficina.Value;
                          dat[5] = representante.Value;
                          dat[6] = correoOficina.Value;
-                         dat[7] = telefonoOficina.Value;
-                         dat[8] = ll;
+                         dat[7] = telefonoOficina.Value;//tel 1
+                         dat[8] = ll;//cedula lider
+                        dat[9]="";//nombre lider
+                        dat[10] = 0;//tel2.Value;//telefono 2
+                        
                          controladoraProyecto.ejecutarProyecto(1, dat, vacio);
 
                          if (!string.IsNullOrWhiteSpace(tel2.Value))
@@ -231,6 +234,7 @@ namespace WebApplication1
             string email = correoOficina.Value;
             string telOf = telefonoOficina.Value;*/
 
+          
 
             char est = barraEstado.Value[0];
             Object[] dat = new Object[8];
@@ -316,18 +320,10 @@ namespace WebApplication1
 
         protected void TextBox2_TextChanged(object sender, EventArgs e)
         {
-            nombreProyecto.Value = "jjj";
-
             DataTable dtProyecto = controladoraProyecto.consultar_Total_ProyectoFiltro(TextBox2.Text);
             DataView dvProyecto = dtProyecto.DefaultView;
             gridProyecto.DataSource = dvProyecto;
             gridProyecto.DataBind();
-
-            /*DataTable dtProyecto = new DataTable();
-            DataView dv = new DataView(dtProyecto);
-            gridProyecto.DataSource = dv;*/
-
-
 
         }
         protected void gridProyecto_RowDataBound(object sender, GridViewRowEventArgs e)
@@ -343,7 +339,6 @@ namespace WebApplication1
 
         protected void OnSelectedIndexChanged(object sender, EventArgs e)
         {
-
             foreach (GridViewRow row in gridProyecto.Rows)
             {
                 if (row.RowIndex == gridProyecto.SelectedIndex)
@@ -355,8 +350,46 @@ namespace WebApplication1
                     string nombreProy = row.Cells[0].Text;
                     EntidadProyecto proy = controladoraProyecto.consultarProyecto(nombreProy);
                     nombreProyecto.Value = nombreProy;
-                    objetivo.Text = proy.getObjetivo();
-                    //barraEstado.Value = (proy.getEstado()).ToString();
+                    objetivo.Text = proy.getObjetivo();                   
+                    string estado = (proy.getEstado()).ToString();
+                    barraEstado.Items.Clear();
+                    lider.Items.Clear();
+
+                    string liderP = (proy.getLider()).ToString();            
+
+                    string nombreL = proy.getNombreLider();
+
+                    lider.Items.Add(new ListItem(liderP+ " " +nombreL));
+
+                    switch (estado.ElementAt(0))
+                    {
+                        
+                        case 'p':
+                            {
+                                barraEstado.Items.Add(new ListItem("Pendiente"));                                                             
+                            }
+                            break;
+                        case 'a':
+                            {
+                                barraEstado.Items.Add(new ListItem("Asignado"));
+                            }
+                            break;
+                        case 'e':
+                            {
+                                barraEstado.Items.Add(new ListItem("En Ejecución"));
+                            }
+                            break;
+                        case 'f':
+                            {
+                                barraEstado.Items.Add(new ListItem("Finalizado"));
+                            }
+                            break;
+                        case 'c':
+                            {
+                                barraEstado.Items.Add(new ListItem("Cancelado"));
+                            }
+                            break;
+                    }
                     //lider.Value = (proy.getLider()).ToString();
                     nombreOficina.Value = proy.getNomOf();
                     correoOficina.Value = proy.getCorreoOf();
