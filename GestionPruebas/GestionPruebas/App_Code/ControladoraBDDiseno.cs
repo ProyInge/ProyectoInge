065481cab796737515eeb64d1b2778199f51a6a7
David Solano <desolanom@gmail.com>
2015-10-27 09:36:24 -0600

    safeGets

--- GestionPruebas/GestionPruebas/App_Code/ControladoraBDDiseno.cs
+++ GestionPruebas/GestionPruebas/App_Code/ControladoraBDDiseno.cs
@@ -2,15 +2,73 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.SqlClient;
 
 namespace GestionPruebas.App_Code
 {
     public class ControladoraBDDiseno
     {
-        AccesoBaseDatos baseDatos;
+        private AccesoBaseDatos baseDatos;
         public ControladoraBDDiseno()
         {
             baseDatos = new AccesoBaseDatos();
+        }
+
+        /** 
+         * Descripción: Obtiene el campo String de la tabla de forma segura (revisando si es null o no antes de leerlo)
+         * Recibe un SqlDataReader con el que se obtiene el campo y el índice de la columna a consultar
+         * Devuelve un valor String dependiendo del resultado de la consulta. String.empty si el campo está nulo
+         */
+        public static string SafeGetString(SqlDataReader reader, int colIndex)
+        {
+            if (!reader.IsDBNull(colIndex))
+                return reader.GetString(colIndex);
+            else
+                return string.Empty;
+        }
+
+        /** 
+         * Descripción: Obtiene el campo entero de la tabla de forma segura (revisando si es null o no antes de leerlo)
+         * Recibe un SqlDataReader con el que se obtiene el campo y el índice de la columna a consultar
+         * Devuelve un valor entero dependiendo del resultado de la consulta. -1 si el campo está nulo
+         */
+        public static int SafeGetInt32(SqlDataReader reader, int colIndex)
+        {
+            if (!reader.IsDBNull(colIndex))
+                return reader.GetInt32(colIndex);
+            else
+                return -1;
+        }
+
+        /** 
+         * Descripción: Obtiene el campo fecha de la tabla de forma segura (revisando si es null o no antes de leerlo)
+         * Recibe un SqlDataReader con el que se obtiene el campo y el índice de la columna a consultar
+         * Devuelve un valor DateTime dependiendo del resultado de la consulta. La fecha actual si el campo está nulo
+         */
+        public static DateTime SafeGetDateTime(SqlDataReader reader, int colIndex)
+        {
+            if (!reader.IsDBNull(colIndex))
+                return reader.GetDateTime(colIndex);
+            else
+                return DateTime.Now;
+        }
+
+        /** 
+         * Descripción: Obtiene el campo fecha de la tabla de forma segura (revisando si es null o no antes de leerlo)
+         * Recibe un SqlDataReader con el que se obtiene el campo y el índice de la columna a consultar
+         * Devuelve un valor DateTime dependiendo del resultado de la consulta. La fecha actual si el campo está nulo
+         */
+        public static DateTime SafeGetDate(SqlDataReader reader, int colIndex)
+        {
+            if (!reader.IsDBNull(colIndex))
+                return reader.GetDateTime(colIndex);
+            else
+                return DateTime.Today;
+        }
+
+        public int consultaDiseno(int id)
+        {
+            return 0;
         }
     }
 }
