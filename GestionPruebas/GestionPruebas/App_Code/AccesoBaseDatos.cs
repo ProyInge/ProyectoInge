2debc377844171085e5554eacbb66e8b22cd750c
David Solano <desolanom@gmail.com>
2015-10-27 10:41:25 -0600

    AccesoBD V2.1

--- GestionPruebas/GestionPruebas/App_Code/AccesoBaseDatos.cs
+++ GestionPruebas/GestionPruebas/App_Code/AccesoBaseDatos.cs
@@ -18,8 +18,8 @@ namespace GestionPruebas.App_Code
     public class AccesoBaseDatos
     {
         /*En Initial Catalog se agrega la base de datos propia. Intregated Security es para utilizar Windows Authentication*/
-        //string conexion = "Server=eccibdisw; Initial Catalog= g4inge; Integrated Security=SSPI";
-        string conexion = "Server=dave-pc\\eccibdisw; Initial Catalog= g4inge; Integrated Security=SSPI";
+        string conexion = "Server=eccibdisw; Initial Catalog= g4inge; Integrated Security=SSPI";
+        //string conexion = "Server=dave-pc\\eccibdisw; Initial Catalog= g4inge; Integrated Security=SSPI";
         //string conexion = "Server=DESKTOP-FRM9QAR\\SQLEXPRESS; Initial Catalog= g4inge; Integrated Security=SSPI";
         //string conexion = "Server=DANIEL\\LOCAl; Initial Catalog= g4inge; Integrated Security=SSPI";
         SqlConnection conSQL;
@@ -45,7 +45,13 @@ namespace GestionPruebas.App_Code
          */
         ~AccesoBaseDatos()
         {
-            conSQL.Close();
+            try { 
+                conSQL.Close();
+            }
+            catch
+            {
+
+            }
         }
 
         public string Conexion
