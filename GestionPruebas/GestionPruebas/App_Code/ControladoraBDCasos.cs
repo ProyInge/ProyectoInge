780d414f0ffa6fba402a9799007d067309b44319
GaboBermudez <gbermudezmora@gmail.com>
2015-10-30 11:02:36 -0600

    EntidadCaso y ControlBDCaso

--- GestionPruebas/GestionPruebas/App_Code/ControladoraBDCasos.cs
+++ GestionPruebas/GestionPruebas/App_Code/ControladoraBDCasos.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,81 @@ namespace GestionPruebas.App_Code
 {
     public class ControladoraBDCasos
     {
+        //Clase que controla el acceso a la base de datos
+
+        private AccesoBaseDatos baseDatos;
+
+        public AccesoBaseDatos BaseDatos
+        {
+            get { return baseDatos; }
+            set { baseDatos = value; }
+        }
+
+        /*
+         * Descripción: Constructor por defecto de la controladora.
+         * Requiere: Nada.
+         * Retorna: La controladora nueva.
+         */
+        public ControladoraBDCasos()
+        {
+            BaseDatos = new AccesoBaseDatos();
+        }
+
+        /** 
+         * Descripción: Obtiene el campo String de la tabla de forma segura (revisando si es null o no antes de leerlo)
+         * Recibe un SqlDataReader con el que se obtiene el campo y el índice de la columna a consultar
+         * Devuelve un valor String dependiendo del resultado de la consulta. String.empty si el campo está nulo
+         */
+        public static string SafeGetString(SqlDataReader reader, int colIndex)
+        {
+            if (!reader.IsDBNull(colIndex))
+                return reader.GetString(colIndex);
+            else
+                return string.Empty;
+        }
+
+        /** 
+          * Descripción: Obtiene el campo entero de la tabla de forma segura (revisando si es null o no antes de leerlo)
+          * Recibe un SqlDataReader con el que se obtiene el campo y el índice de la columna a consultar
+          * Devuelve un valor entero dependiendo del resultado de la consulta. -1 si el campo está nulo
+          */
+        public static int SafeGetInt32(SqlDataReader reader, int colIndex)
+        {
+            if (!reader.IsDBNull(colIndex))
+                return reader.GetInt32(colIndex);
+            else
+                return -1;
+        }
+
+        /*
+         * Descripción: Inserta un caso en la BD.
+         * Recibe:
+         * Retorna: n/a.
+         */
+        public string insertarCaso(EntidadCasos caso)
+        {
+            string resultado = "Exito";
+            string consulta = "";
+
+            try
+            {
+                consulta = "INSERT INTO CasoPrueba (id,proposito,tipoEntrada,resultadoEsperado,flujoCentral, idDise) VALUES (@0, @1, @2, @3);";
+                Object[] args = new Object[4];
+                args[0] = caso.Id;
+                args[1] = caso.Proposito;
+                args[2] = caso.TipoEntrada;
+                args[3] = caso.ResultadoEsperado;
+                args[4] = caso.FlujoCentral;
+                args[5] = caso.IdDise;
+                SqlDataReader res = baseDatos.ejecutarConsulta(consulta, args);
+                res.Close();
+            }
+            catch (SqlException ex)
+            {
+                resultado = "Error al insertar. Error 1: " + ex.Message;
+            }
+
+            return resultado;
+        }
     }
 }
