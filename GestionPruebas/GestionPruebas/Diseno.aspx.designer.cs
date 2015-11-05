333631a9b838faf2a6a59ebe60d7f3693796b4a1
David Solano <desolanom@gmail.com>
2015-11-05 17:27:49 -0600

    diseno validation (en proceso)

--- GestionPruebas/GestionPruebas/Diseno.aspx.designer.cs
+++ GestionPruebas/GestionPruebas/Diseno.aspx.designer.cs
@@ -193,6 +193,15 @@ namespace GestionPruebas {
         protected global::System.Web.UI.WebControls.DropDownList responsable;
         
         /// <summary>
+        /// Control RequiredFieldValidator1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator1;
+        
+        /// <summary>
         /// Control btnAceptarDiseno.
         /// </summary>
         /// <remarks>
